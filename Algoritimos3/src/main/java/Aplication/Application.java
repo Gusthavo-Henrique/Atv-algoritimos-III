7e2bc984fd057fbacd14fb82c60c55ac5651d80c
Gusthavo-Henrique <125114800+Gusthavo-Henrique@users.noreply.github.com>
2023-06-15 12:51:36 -0300

    questao 7

--- Algoritimos3/src/main/java/Aplication/Application.java
+++ Algoritimos3/src/main/java/Aplication/Application.java
@@ -1,8 +1,7 @@
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -15,30 +14,36 @@ public class Main {
             FileReader reader = new FileReader("C:\\Users\\Umpalumpa\\eclipse-workspace\\Algoritimos3\\target\\teste1.json");
             List<Aluno> alunos = gson.fromJson(reader, new TypeToken<List<Aluno>>() {}.getType());
             
-            // Encontrar o aluno pelo CPF
-            String cpfBuscado = "111.111.111-11";
-            Aluno alunoBuscado = null;
+            // Mapa para armazenar o total de reprovações por disciplina
+            Map<String, Integer> reprovasPorDisciplina = new HashMap<>();
             
+            // Iterar sobre os alunos e contar as reprovações por disciplina
             for (Aluno aluno : alunos) {
-                if (aluno.getCpf().equals(cpfBuscado)) {
-                    alunoBuscado = aluno;
-                    break;
+                if (aluno.getSemestre().equals("2023.1")) {
+                    List<Disciplina> disciplinas = aluno.getDisciplinas();
+                    for (Disciplina disciplina : disciplinas) {
+                        if (!disciplina.isAprovado()) {
+                            reprovasPorDisciplina.put(disciplina.getNome(), 
+                                                      reprovasPorDisciplina.getOrDefault(disciplina.getNome(), 0) + 1);
+                        }
+                    }
                 }
             }
             
-            if (alunoBuscado != null) {
-                // Ordenar os alunos pela média em ordem decrescente
-                Collections.sort(alunos, Comparator.comparing(Aluno::getMediaGeral).reversed());
-                
-                // Encontrar a posição do aluno buscado no ranking
-                int posicao = alunos.indexOf(alunoBuscado) + 1;
-                
-                // Exibir o ranking do aluno
-                System.out.println("Ranking do aluno " + alunoBuscado.getNome() + " (CPF: " + cpfBuscado + ") no semestre 2023.1:");
-                System.out.println("Posição: " + posicao + " de " + alunos.size());
-            } else {
-                System.out.println("Aluno não encontrado.");
+            // Encontrar a disciplina que mais reprova
+            String disciplinaMaisReprova = null;
+            int maxReprovacoes = 0;
+            
+            for (Map.Entry<String, Integer> entry : reprovasPorDisciplina.entrySet()) {
+                if (entry.getValue() > maxReprovacoes) {
+                    disciplinaMaisReprova = entry.getKey();
+                    maxReprovacoes = entry.getValue();
+                }
             }
+            
+            // Exibir a disciplina que mais reprova
+            System.out.println("Disciplina que mais reprova no semestre 2023.1:");
+            System.out.println(disciplinaMaisReprova + " (Total de reprovações: " + maxReprovacoes + ")");
             
         } catch (Exception e) {
             e.printStackTrace();
